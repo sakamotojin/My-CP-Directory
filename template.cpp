eef40a0888fb8a5f85bfafdf2f1cc41bde98be71
Sumit Joshi <sumit151997@gmail.com>
2018-12-22 17:20:12 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+#define F first
+#define S second
+# define ss(v) v.begin(),v.end()
+#define pb push_back
+#define mk make_pair
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define pr pair<ll ,ll> 
+#define  vpr vector<pr>
+const long long int mod=998244353;
+const long double EPS=1e-12;
+ll mulp(ll a, ll b){ return ((a % mod) * (b % mod)) % mod; }
+ll add(ll a, ll b) {     return ((a % mod) + (b % mod)) % mod; }
+ll sub(ll a, ll b) { return ((a%mod) - (b % mod)+ mod) % mod ; }
+int solve()
+{
+	
+}
+int main()
+{
+	fast;
+	ll t;
+	t=1;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+
+	return 0;
+}
